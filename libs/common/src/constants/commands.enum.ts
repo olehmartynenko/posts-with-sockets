22429d0b8dabf9e8323b6aa750d6c6738ebe0868
olehmartynenko <168522056+omartynenkocodebridge@users.noreply.github.com>
2024-09-07 16:50:51 +0300

    refactor: Refactor constants

--- libs/common/src/constants/commands.enum.ts
+++ libs/common/src/constants/commands.enum.ts
@@ -1,7 +1,5 @@
-enum Commands {
-  GET_USERS = 'get-users',
-  GET_USER_POSTS = 'get-user-posts',
-  GET_POST_COMMENTS = 'get-post-comments',
-}
-
-export default Commands;
+export const Commands = {
+  GET_USERS: 'get-users',
+  GET_USER_POSTS: 'get-user-posts',
+  GET_POST_COMMENTS: 'get-post-comments',
+} as const;
