d83e40c22f0db5a26e48555f2b33a122dceb9ad9
olehmartynenko <168522056+omartynenkocodebridge@users.noreply.github.com>
2024-09-07 16:54:28 +0300

    Add command for write API

--- libs/common/src/constants/commands.enum.ts
+++ libs/common/src/constants/commands.enum.ts
@@ -2,4 +2,10 @@ export const Commands = {
   GET_USERS: 'get-users',
   GET_USER_POSTS: 'get-user-posts',
   GET_POST_COMMENTS: 'get-post-comments',
+  CREATE_USER: 'create-user',
+  UPDATE_USER: 'update-user',
+  CREATE_POST: 'create-post',
+  UPDATE_POST: 'update-post',
+  CREATE_COMMENT: 'create-comment',
+  UPDATE_COMMENT: 'update-comment',
 } as const;
