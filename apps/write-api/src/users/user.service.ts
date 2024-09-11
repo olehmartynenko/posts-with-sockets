838a0a12932773e487602a23dab2ae191e984a4b
olehmartynenko <168522056+omartynenkocodebridge@users.noreply.github.com>
2024-09-11 09:00:03 +0300

    feat: Improve caching on user updating

--- apps/write-api/src/users/user.service.ts
+++ apps/write-api/src/users/user.service.ts
@@ -20,8 +20,6 @@ export class UserService {
 
     const cachedUsersKeys = await this.cacheService.listKeys('users-*');
 
-    console.log(cachedUsersKeys);
-
     if (cachedUsersKeys) {
       for (const key of cachedUsersKeys) {
         // Here we can just invalidate the cache for all users because we only know if the new user matches the filter
@@ -50,14 +48,23 @@ export class UserService {
         // If the user does not match previous filter, invalidate the cache
         if (query.email && !user.email.includes(query.email)) {
           this.cacheService.delete(key);
+          continue;
         }
 
         // If the user does not match previous filter, invalidate the cache
         if (query.name && !user.name.includes(query.name)) {
           this.cacheService.delete(key);
+          continue;
         }
 
         const cachedUsers = (await this.cacheService.get<UserDto[]>(key)) ?? [];
+
+        // If the user didnt match current filter, but now match, invalidate the cache
+        // This is because we dont know where the user should be placed in terms of pagination
+        if (!cachedUsers.find((cachedUser) => cachedUser.id === user.id)) {
+          this.cacheService.delete(key);
+          continue;
+        }
 
         await this.cacheService.set<UserDto[]>(
           key,
